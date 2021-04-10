2787b6fe5d576af23dc31f922c328d476cd22ce8
Ana Laura Fernandes de Oliveira <ana.oliveira@4mti.com.br>
2021-04-10 01:46:30 -0300

    Evolução da tela de letras com a implementação de slide show de imagens

--- lib/segunda_tela.dart
+++ lib/segunda_tela.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'utilitarios.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_options.dart';
+
+final List<String> imgList = [
+'./images/letraA.JPG','./images/letraB.JPG','./images/letraC.JPG', './images/letraD.JPG',
+  './images/letraE.JPG', './images/letraF.JPG', './images/letraG.JPG', './images/letraH.JPG',
+  './images/letraI.JPG', './images/letraJ.JPG', './images/letraK.JPG', './images/letraL.JPG',
+  './images/letraM.JPG', './images/letraN.JPG', './images/letraO.JPG', './images/letraP.JPG',
+  './images/letraQ.JPG', './images/letraR.JPG', './images/letraS.JPG', './images/letraT.JPG',
+  './images/letraU.JPG', './images/letraV.JPG', './images/letraW.JPG', './images/letraX.JPG',
+  './images/letraY.JPG', './images/letraZ.JPG'
+];
 
 class SegundaTela extends StatefulWidget {
   @override
@@ -11,62 +23,17 @@ class _SegundaTelaState extends State<SegundaTela> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.white),
-          actions: <Widget>[
-            PopupMenuButton(itemBuilder: (BuildContext context){
-              return[
-                PopupMenuItem(child: Text('Flutter')),
-                PopupMenuItem(child: Text('Android')),
-              ];
-            })
-          ],
-        ),
-        body: new Stack(
-            children: <Widget>[
-              new Container(
-                  decoration: new BoxDecoration(
-                      image: new DecorationImage(
-                          image: AssetImage('./images/background_default.png'),
-                          fit: BoxFit.cover))
+      appBar: AppBar(title: Text('Image slider demo')),
+      body: Container(
+          child: CarouselSlider(
+            options: CarouselOptions(),
+            items: imgList.map((item) => Container(
+              child: Center(
+                  child: Image.asset(item, fit: BoxFit.cover, width: 1000)
               ),
-              new Center(
-                  child: Padding(
-                      padding: EdgeInsets.fromLTRB(128, 0, 128, 0),
-                      child: Container(
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                      icon: Icon(Icons.person),
-                                      border: UnderlineInputBorder(),
-                                      labelText: 'Enter your username'
-                                  ),
-                                ),
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                      icon: Icon(Icons.lock),
-                                      border: UnderlineInputBorder(),
-                                      labelText: 'Enter your password'
-                                  ),
-                                ),
-                                Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                                    child: botaoGenerico("Entrar", context, "/segunda_tela")
-                                )
-
-                              ]
-                          )
-                      )
-                  )
-              )
-            ]
-        )
+            )).toList(),
+          )
+      ),
     );
   }
 }
